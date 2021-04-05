0b9cdb5a3a285fb20646eeee6c90131e567f3964
Romain LAGARDETTE <romain.l@yoopala.com>
2021-04-05 18:22:51 +0200

    Add project files.

---
+++ TDD.Partiel01.LibTests/PurchaseValidationTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace TDD.Partiel01.LibTests
+{
+    public class PurchaseValidationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Le client valide son achat.
+            //Le client a :
+            //- un panier de 3 articles
+            //- sélectionné son adresse de livraison
+            //- sélectionné le mode paiement CB
+            //Lorsque le client veut valider son achat
+            //Mais que sa banque rejette le paiement
+            //Alors l’erreur de la banque est retournée et l’achat n’est pas validé
+
+
+
+        }
+    }
+}
