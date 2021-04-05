9870caba8819227f2ef4986cd6b3b1065c5f6716
Romain LAGARDETTE <romain.l@yoopala.com>
2021-04-05 22:33:06 +0200

    refacto

--- TDD.Partiel01.LibTests/InMemoryCreditCardPayment.cs
+++ TDD.Partiel01.LibTests/InMemoryCreditCardPayment.cs
@@ -7,9 +7,15 @@ namespace TDD.Partiel01.LibTests
 {
     public class InMemoryCreditCardPayment : ICreditCardPayment
     {
+        private List<string> accountInTheRedNumber = new List<string>
+        {
+            "9745965412543654",
+            "1265599754346544"
+        };
+
         public bool Process(CreditCardDetails creditCardDetails)
         {
-            return creditCardDetails.Number != "9745965412543654" && creditCardDetails.Number != "1265599754346544";
+            return !accountInTheRedNumber.Contains(creditCardDetails.Number);
         }
     }
 }
