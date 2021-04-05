bab37b893ffdf8c8542edcfbba3144192683c9af
Romain LAGARDETTE <romain.l@yoopala.com>
2021-04-05 18:32:26 +0200

    add one acceptance test

--- TDD.Partiel01.LibTests/PurchaseValidationTests.cs
+++ TDD.Partiel01.LibTests/PurchaseValidationTests.cs
@@ -5,19 +5,16 @@ namespace TDD.Partiel01.LibTests
 {
     public class PurchaseValidationTests
     {
+        //Anna a : un panier de 2 articles, sélectionné son adresse de livraison, sélectionné mode paiement CB.
+        //Lorsque Anna valide son achat
+        //Mais que sa banque rejette le paiement pour solde insuffisant
+        //Alors l’erreur de la banque est retournée et l’achat n’est pas validé
+
+
         [Fact]
         public void Test1()
         {
-            //Le client valide son achat.
-            //Le client a :
-            //- un panier de 3 articles
-            //- sélectionné son adresse de livraison
-            //- sélectionné le mode paiement CB
-            //Lorsque le client veut valider son achat
-            //Mais que sa banque rejette le paiement
-            //Alors l’erreur de la banque est retournée et l’achat n’est pas validé
-
-
+    
 
         }
     }
